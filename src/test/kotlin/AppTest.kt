cbdfb2a1eca855eef67a18b01d64426af4a65162
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-06-06 21:13:25 +1000

    Give more readable syntax for assertion

--- src/test/kotlin/AppTest.kt
+++ src/test/kotlin/AppTest.kt
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class AppTest {
+    private infix fun <T> T.equalsTo(expected: T) = assertEquals(expected, this)
+
     @Test
     fun `find who to greet`() = runBlocking {
-        assertEquals(getWhoToGreet(), "World!")
+        getWhoToGreet() equalsTo "World!"
     }
 }
