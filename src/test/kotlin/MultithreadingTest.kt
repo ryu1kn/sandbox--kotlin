2cdb9e0b281c51dad32677c35a994ba744061114
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-06-09 23:26:05 +1000

    Actor version goes slower than synchronized version as the # of iteration increase

--- src/test/kotlin/MultithreadingTest.kt
+++ src/test/kotlin/MultithreadingTest.kt
@@ -1,10 +1,8 @@
 import helper.AssertionHelper
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
-import kotlinx.coroutines.withContext
 import org.junit.jupiter.api.Test
 import kotlin.system.measureTimeMillis
 
@@ -19,7 +17,7 @@ class MultithreadingTest : AssertionHelper {
 
         val time = measureTimeMillis {
             withContext(Dispatchers.Default) {
-                (1..iteration).map {
+                (1..iteration).forEach { _ ->
                     launch {
                         synchronized(threadNames) {
                             threadNames.add(Thread.currentThread().name)
@@ -41,7 +39,7 @@ class MultithreadingTest : AssertionHelper {
 
         val time = measureTimeMillis {
             withContext(Dispatchers.Default) {
-                (1..iteration).map {
+                (1..iteration).forEach { _ ->
                     mutex.withLock {
                         threadNames.add(Thread.currentThread().name)
                     }
@@ -53,4 +51,40 @@ class MultithreadingTest : AssertionHelper {
         println(countThreads(threadNames))
         println("Time taken: $time")
     }
+
+    @ObsoleteCoroutinesApi
+    @Test
+    fun `Use multiple threads - Actor`() = runBlocking<Unit> {
+        fun CoroutineScope.createThreadNameActor() = actor<ThreadNameMsg> {
+            val threadNames = mutableListOf<String>()
+            for (msg in channel) when (msg) {
+                is CollectThreadNameMsg -> threadNames.add(Thread.currentThread().name)
+                is GetCollectedThreadNames -> msg.result.complete(threadNames)
+            }
+        }
+
+        val collector = createThreadNameActor()
+        val time = measureTimeMillis {
+            withContext(Dispatchers.Default) {
+                (1..iteration).forEach { _ ->
+                    launch {
+                        collector.send(CollectThreadNameMsg)
+                    }
+                }
+            }
+
+            val result = CompletableDeferred<List<String>>()
+            collector.send(GetCollectedThreadNames(result))
+
+            val threadNames = result.await()
+            threadNames.size equalsTo iteration
+            println(countThreads(threadNames))
+        }
+        println("Time taken: $time")
+        collector.close()
+    }
+
+    private interface ThreadNameMsg
+    private object CollectThreadNameMsg : ThreadNameMsg
+    private class GetCollectedThreadNames(val result: CompletableDeferred<List<String>>) : ThreadNameMsg
 }
